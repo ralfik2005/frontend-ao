a2994623a1b54517ba69f4b13aeb5097aa5bb541
ralfik2005 <and.kuc@outlook.com>
2022-05-20 16:48:41 +0100

    Hello two

--- hello.php
+++ hello.php
@@ -1,9 +1,17 @@
 <html>
     <head>
+<<<<<<< HEAD
         <title>PHP Hello World!</title>
+=======
+        <title>PHP Hello Stay Private World!</title>
+>>>>>>> Hello two
     </head>
     <body>
         <?php echo '<h1>Hello World</h1>'; ?>
         <?php phpinfo(); ?>
     </body>
+<<<<<<< HEAD
 </html>
+=======
+</html>
+>>>>>>> Hello two
