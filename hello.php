804f055b01154cda6c17e3b4fc536952d7f00c73
ralfik2005 <and.kuc@outlook.com>
2022-06-09 09:42:48 +0100

    New message

--- hello.php
+++ hello.php
@@ -7,7 +7,7 @@
 >>>>>>> Hello two
     </head>
     <body>
-        <?php echo '<h1>Hello World</h1>'; ?>
+        <?php echo '<h1>Hello StayPrivate;)</h1>'; ?>
         <?php phpinfo(); ?>
     </body>
 <<<<<<< HEAD
