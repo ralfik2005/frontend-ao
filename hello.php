a3282484c668b2c4730bf9238d2b37986d7fb084
ralfik2005 <and.kuc@outlook.com>
2022-06-10 11:54:43 +0100

    meeee

--- hello.php
+++ hello.php
@@ -1,18 +1,10 @@
 <html>
     <head>
-<<<<<<< HEAD
         <title>PHP Hello World!</title>
-=======
-        <title>PHP Hello Stay Private World!</title>
->>>>>>> Hello two
     </head>
     <body>
         <?php echo '<h1>Hello form Stay Private 
-        Test ;)</h1>'; ?>
+        Test World;)</h1>'; ?>
         <?php phpinfo(); ?>
     </body>
-<<<<<<< HEAD
 </html>
-=======
-</html>
->>>>>>> Hello two
